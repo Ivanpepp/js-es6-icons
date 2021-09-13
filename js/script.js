1f1e1013a7c9878debff7580f49d6f93e4ceb773
Ivanpepp <nastroivan3@gmail.com>
2021-09-13 17:52:04 +0200

    mileston 2 completed

--- js/script.js
+++ js/script.js
@@ -97,23 +97,72 @@ const icons = [
 	}
 ];
 
+//mettiamo i tipi nella variabile types
+const types = getProrperty(icons, 'type');
+// creiamo un array di colori 
+const color = ['red', 'green', 'blue', 'black', 'yellow'];
+let colorizedIcons = colorizeItems(icons, color);
+
 /* Milestone 1
 Partendo dalla seguente struttura dati , mostriamo in pagina tutte
 le icone disponibili come da layout. */
 const container = document.querySelector('#icone');
-mostraInPagina(icons, container);
+mostraInPagina(colorizedIcons, container);
 
 function mostraInPagina(array, container){
 
     array.forEach((element) => {
 
-        const {name, prefix, type, family} = element;
+        const {name, prefix, type, family, color} = element;
 
         container.innerHTML += 
         `<div>
-            <i class="${family} ${prefix}${name}"></i>
+            <i class="${family} ${prefix}${name}"  style="color: ${color}"></i>
             <h4>${name} (${type})</h4>
         </div>
         `;
     });
+}
+/* Milestone 2
+Coloriamo le icone per tipo */
+
+
+
+
+
+/* adesso ci facciamo una funzione che prendete e ci da tutti i tipi univoci dell'array */
+// in quesot modo nella property mettiamo la proprieta che vogliamor icercare in questo modo e` piu` generico
+// inserendo nel primo parametro l'array e nel secondo 'la proprieta'
+function getProrperty(array, property) {
+
+    //array dove andranno tutti tipi
+    const types = [];
+
+    array.forEach((element) => {
+        if(! types.includes(element[property])){
+            types.push(element[property])
+        }
+    });
+
+    return types;
+}
+
+
+//adesso creiamo una funzione dove coloroare gli items in base al tipo
+
+function colorizeItems(array, colors){
+    const types = getProrperty(array, 'type');
+
+    const colorizedArray = array.map((element)=>{
+        //salviamo in una variabile locale l'indice dell'elemento element.type
+        const indexOfType = types.indexOf(element.type);
+        //se e presente idnexoftype sara diverso da 1 e quindi
+        if(indexOfType !== -1){
+            //inseriesco come nuova proprieta -color- il colore allos tesso indice
+            //del nostro tiop nell'array dei tipi
+            element.color = colors[indexOfType];
+        }
+        return element;
+    })
+    return colorizedArray;
 }
